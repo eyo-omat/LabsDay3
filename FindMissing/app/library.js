f9b8f18e69483248069417844cd877f754a85cc8
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-11 09:29:39 +0100

    update findMissing function to pass test cases

--- FindMissing/app/library.js
+++ FindMissing/app/library.js
@@ -3,6 +3,27 @@
 module.exports = {
 
 findMissing: function(arr1, arr2){
-    if ((arr1.length < 1) || (arr2.length < 1)) { return 0; }
+var array1Length = arr1.length;
+var array2Length = arr2.length;
+var numberPresent = function (y, arr) {
+	for (var z in arr){
+		if (y === z) { 
+			return true; 
+		} 
+	return false;
+	}
+}
+    if ((array1Length < 1) || (array2Length < 1)) { return 0; }
+    for (var x in arr1){
+    	if (!numberPresent(x, arr2)) {
+    		return x;
+		} else { 
+			return 0; 
+		}
+	}
+
+
+
+
 }
 }
