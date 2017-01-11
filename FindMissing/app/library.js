e1cfb35fc50c9d86f135bba47aca842bf527c8d0
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-11 16:32:26 +0100

    add index file

--- FindMissing/app/library.js
+++ FindMissing/app/library.js
@@ -5,23 +5,19 @@ module.exports = {
 findMissing: function(arr1, arr2){
 var array1Length = arr1.length;
 var array2Length = arr2.length;
-var missingNumber = 0;
-var numberPresent = function (y, arr) {
-	for (var z=0; z<arr.length; z++){
-		if (y === arr[z]) { 
-			return true; 
-		} 
-	return false;
-	}
-};
+var arr1S = 0;
+var arr2S = 0; 
+
     if ((array1Length < 1) || (array2Length < 1)) { return 0; }
-    for (var x=0; x<array2Length; x++){
-    	if (!numberPresent(arr2[x], arr1)) {
-    		missingNumber = arr2[x];
-    		break;
-		}
+
+	for (var z=0; z<array1Length; z++){
+		arr1S += arr1[z];
 	}
-return missingNumber;
+
+    for (var x=0; x<array2Length; x++){
+    	arr2S += arr2[x];
+	}
+return missingNumber = arr2S - arr1S;
 
 }
 }
