ae07d0056a43f320c3030f08e5bf660e28022303
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-11 10:55:11 +0100

    add test cases for array prototype project

---
+++ ArrayProtoype/app/library.js
@@ -0,0 +1,6 @@
+Array.prototype.indexOf = function () {
+  throw new Error('You are not allowed to use the indexOf function');
+};
+var oneToTwenty = [].toTwenty();
+var twoToForty = [].toForty();
+var tenToOneThousand = [].toOneThousand();
